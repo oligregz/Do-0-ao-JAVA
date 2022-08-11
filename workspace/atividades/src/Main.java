f8abf31e5d3adf52c67ced90a8500ab429152dca
Gregory <gregorysak90@gmail.com>
2022-08-11 11:36:02 -0300

    feat: implementando formas diferentes de imprir um valor

--- workspace/atividades/src/Main.java
+++ workspace/atividades/src/Main.java
@@ -2,7 +2,18 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world! Haha");
-	}
+		System.out.print("Hello world!");
+		System.out.println(" Haha");
+		
+		int y = 32;
+		double x = 10.645264;
+		
+		System.out.println(y);
+		System.out.println(x);
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
+		System.out.println("Foi");
+		
+		}
 
 }
