80e314d2347a060c1890d1f82561d087039c4f41
gregory <gregorysak90@gmail.com>
2022-08-26 20:44:06 -0300

    feat: exercicio de teste do sexto bit com mascara implementado

--- workspace/atividades/src/other_basic_topics_java/other_basic_topcs_java/src/main_other_basic_topcs_java.java
+++ workspace/atividades/src/other_basic_topics_java/other_basic_topcs_java/src/main_other_basic_topcs_java.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
 
+// testando sexto bit com uma mascara
 public class main_other_basic_topcs_java {
 
 	public static void main(String[] args) {
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite um numero:");
+		// 89 e 113 sao numeros para teste
+		
+		int n = sc.nextInt();
+		int mask = 0b100000; // poderia ser 32 no lugar do numero
+		
+		if ((n & mask) != 0) {
+			System.out.println("O sexto bit e verdadeiro.");
+		} else {
+			System.out.println("O sexto bit e falso.");
+		}
+		sc.close();
 	}
-
 }
