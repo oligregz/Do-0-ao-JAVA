e8a4aa6aeb3b20c986f49fb786f31c18a7a6693f
gregory <gregorysak90@gmail.com>
2022-10-04 08:48:18 -0300

    feat: mudando metodos e implementando novos em banckaccount

--- workspace/atividades/src/class_characteristics/study-class-charcterisctics/src/entities/BankAccount.java
+++ workspace/atividades/src/class_characteristics/study-class-charcterisctics/src/entities/BankAccount.java
@@ -4,20 +4,19 @@ public class BankAccount {
 
 	private int accountNumber;
 	private String accountHolder;
-	private double deposit;
 	private double balance;
-	
-	public BankAccount(int accountNumber, String accountHolder, double deposit) {
-		this.accountNumber = accountNumber;
-		this.accountHolder = accountHolder;
-		this.deposit = deposit;
-	}
 	
 	public BankAccount(int accountNumber, String accountHolder) {
 		this.accountNumber = accountNumber;
 		this.accountHolder = accountHolder;
-		this.balance = balance;
 	}
+	
+	public BankAccount(int accountNumber, String accountHolder, double initialDeposit) {
+		this.accountNumber = accountNumber;
+		this.accountHolder = accountHolder;
+		deposit(initialDeposit);
+	}
+	
 	
 	public double getAccountNumber() {
 		return accountNumber;
@@ -32,17 +31,24 @@ public class BankAccount {
 	}
 	
 	public double getBalance() {
-		return deposit;
+		return balance;
 	}
 	
-	public void setDeposit(double newDeposit) {
-		this.deposit = newDeposit;
+	public void deposit(double newDeposit) {
+		balance += newDeposit;
 	}
 	
-	public String getData() {
-		return "Account " + accountNumber
-				+ ", Holder " + accountHolder
-				+ ", Balance " + balance;
+	public void withdraw(double amount) {
+		balance -= amount + 5.0;
+	}
+	
+	public String toString() {
+		return "Account " 
+					+ accountNumber
+					+ ", Holder: "
+					+ accountHolder
+					+ ", Balance: "
+					+ String.format("%.2f", balance);
 				
 	}
 }
